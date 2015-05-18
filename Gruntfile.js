d43031fe2a74510be305acbbf3a9aa0aaf79013b
Christopher Hiller <chiller@badwing.com>
2015-05-17 18:37:31 -0700

    fiddle w/ grunt tasks, update .eslintrc, add strip-ansi and pipe-event

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,25 +1,31 @@
 'use strict';
 
 module.exports = function (grunt) {
-  var loadGruntConfig = require('load-grunt-config');
-  var pkg = grunt.file.readJSON('package.json');
+  const loadGruntConfig = require('load-grunt-config'),
+    util = require('util');
+  const pkg = grunt.file.readJSON('package.json');
+  let cfg;
 
   if (grunt.option('time')) {
     require('time-grunt')(grunt);
   }
 
-  loadGruntConfig(grunt, {
+  cfg = loadGruntConfig(grunt, {
+    init: false,
     jitGrunt: {
       staticMappings: {
         devUpdate: 'grunt-dev-update',
         'bump-only': 'grunt-bump',
         'bump-commit': 'grunt-bump',
-        mochacov: 'grunt-mocha-cov',
-        _watch: 'grunt-contrib-watch'
+        mochacov: 'grunt-mocha-cov'
       }
     },
     data: {
       pkg: pkg
     }
   });
+
+  grunt.verbose.ok('Grunt config:\n%s', util.inspect(cfg, {depth: null}));
+
+  grunt.initConfig(cfg);
 };
