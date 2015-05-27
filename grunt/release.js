3f07e9c74553152667bdb851937e1f2061100694
Christopher Hiller <boneskull@boneskull.com>
2015-05-27 00:42:00 -0700

    update digs dep requirement; remove unused grunt task "release"

--- grunt/release.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-module.exports = function (grunt) {
-  grunt.registerTask('release', 'Build & Bump', function (target) {
-    grunt.task.run('bump-only:' + target);
-    grunt.task.run('build');
-    grunt.task.run('bump-commit');
-  });
-};
