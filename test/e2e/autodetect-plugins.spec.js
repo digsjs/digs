193185c8ea79e1bcda63297fa18053df146bbe4f
Christopher Hiller <boneskull@boneskull.com>
2015-06-25 23:40:42 -0700

    successfully loading internal plugins

--- test/e2e/autodetect-plugins.spec.js
+++ test/e2e/autodetect-plugins.spec.js
@@ -14,12 +14,21 @@ describe('plugin autodetection', function () {
     sandbox.restore();
   });
 
-  it('should autodetect plugins', function () {
-    let digs = new Digs({ autoDetectPlugins: true });
+  it('should autodetect internal plugins', function () {
+    let digs = new Digs();
     return digs._autoDetected.then(function () {
       expect(digs._registeredPlugins.digsBroker.func)
         .to.equal(require('digs-broker'));
     });
+  });
+
+  it('should load internal plugins', function () {
+    let digs = new Digs();
+    return digs.start()
+      .then(function () {
+        expect(digs.digsBroker).to.be
+          .instanceOf(require('digs-broker').DigsBroker);
+      });
   });
 
 });
