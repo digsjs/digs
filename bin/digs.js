2f88d7673ea7d86ff51a58400d64e2aa74dd3f3c
Christopher Hiller <boneskull@boneskull.com>
2015-10-19 12:47:33 -0700

    don't expose common definitions; add debug output

--- bin/digs.js
+++ bin/digs.js
@@ -4,21 +4,33 @@
 
 'use strict';
 
-let Digs = require('../lib');
-let yaml = require('yaml-js');
-let yargs = require('yargs');
-let common = require('digs-common');
-let fs = common.fs;
-let Promise = common.Promise;
+const Digs = require('../lib');
+const yaml = require('yaml-js');
+const yargs = require('yargs');
+const common = require('digs-common');
+const fs = common.fs;
+const Promise = common.Promise;
+const debug = require('debug')('digs-cli');
 
 function parseConfig(filepath) {
   if (!filepath) {
     return Promise.resolve();
   }
+  debug('Attempting to read config file...');
   return fs.readFileAsync(filepath, 'utf8')
-    .done(function success(file) {
-      return /\.json$/.test(filepath) ? JSON.parse(file) : yaml.load(file);
-    }, function fail() {
+    .then((file) => {
+      if (/\.json$/.test(filepath)) {
+        debug('Attempting to parse config file as JSON...');
+        return JSON.parse(file);
+      }
+      debug('Attempting to parse config file as YAML...');
+      return yaml.load(file);
+    })
+    .tap(() => {
+      debug('Succesfully parsed config file');
+    })
+    .catch((err) => {
+      debug(err);
       throw new Error(`Cannot read or parse file at "${filepath}"`);
     });
 }
@@ -27,12 +39,25 @@ const argv = yargs
   .option('config', {
     alias: 'c',
     describe: 'Config manifest file (JSON/YAML)',
-    type: 'string'
+    string: true
+  })
+  .option('debug', {
+    boolean: true,
+    describe: 'Enable debugging output (while starting server)'
+  })
+  .check((args) => {
+    if (args.debug) {
+      require('debug').enable('digs-*');
+      debug('Debug mode enabled');
+    }
+    if (args.config) {
+      debug(`Using config at path ${args.config}`);
+    } else {
+      debug('No config file specified');
+    }
   })
   .argv;
 
 parseConfig(argv.config)
-  .then(function createServer(config) {
-    Digs.createServer(config);
-  });
+  .then((config) => Digs.createServer(config));
 
