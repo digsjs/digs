a0ed7c909fd8a5b8a6dad0f7f5db6b8798e71455
Christopher Hiller <boneskull@boneskull.com>
2015-10-25 13:12:18 -0700

    fix code style of label script

--- scripts/sync-github-labels.js
+++ scripts/sync-github-labels.js
@@ -2,67 +2,61 @@
 
 'use strict';
 
-var GitHubApi = require('github');
-var Promise = require('digs-common').Promise;
-var _ = require('digs-common').utils;
-var childProcess = require('child-process-promise');
-var binPath = Promise.promisify(require('bin-path')(require));
-var format = require('util').format;
-var path = require('path');
+const GitHubApi = require('github');
+const Promise = require('digs-common').Promise;
+const _ = require('digs-common').utils;
+const childProcess = require('child-process-promise');
+const binPath = Promise.promisify(require('bin-path')(require));
+const path = require('path');
 
-var github = new GitHubApi({
-  version: "3.0.0",
-  protocol: "https",
-  host: "api.github.com",
+const github = new GitHubApi({
+  version: '3.0.0',
+  protocol: 'https',
+  host: 'api.github.com',
   timeout: 5000,
   headers: {
-    "user-agent": "digsjs-label-sync-script"
+    'user-agent': 'digsjs-label-sync-script'
   }
 });
 
-var project = process.argv[2];
+const project = process.argv[2];
 if (project) {
-  console.log('Updating "digsjs/%s"', project);
+  console.log(`Updating 'digsjs/${project}`, project);
 } else {
   console.log('Querying GitHub for projects...');
 }
 
-
 binPath('github-labels')
   .get('labels')
-  .then(function(executable) {
+  .then((executable) => {
     function update(name) {
       return childProcess.spawn(executable,
         [
           '-c',
           require.resolve('./github-labels.json'),
           '-f',
-          format('digsjs/%s', name)
+          `digsjs/${name}`
         ])
-        .then(function() {
-          console.log('done.');
-        });
+        .then(() => console.log('done.'));
     }
 
-    console.log('Using github-labels executable "%s"',
+    console.log(`Using github-labels executable "%s"`,
       path.relative(process.cwd(), executable));
 
     if (project) {
-      process.stdout.write(format('%s...', project));
+      process.stdout.write(`${project}...`);
       return update(project);
     }
 
     return Promise.promisify(github.repos.getFromOrg)({org: 'digsjs'})
-      .then(function(data) {
-        var names = _.pluck(data, 'name');
-        console.log('Found %d repos to sync.', names.length);
-        return Promise.each(names, function(name) {
-          process.stdout.write(format('%s...', name));
+      .then((data) => {
+        const names = _.pluck(data, 'name');
+        console.log(`Found ${names.length} repos to sync.`);
+        return Promise.each(names, (name) => {
+          process.stdout.write(`${name}...`);
           return update(name);
         });
       });
   })
-  .catch(function(err) {
-    console.log(err);
-  });
+  .catch(err => console.log(err));
 
